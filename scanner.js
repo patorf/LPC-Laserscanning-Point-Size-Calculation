28e5b4694fb7f3133cb7dcfe22503caff49ccb3e
patorf <philippatorf@gmail.com>
2017-01-20 14:17:39 +0100

    add slider and drawing functions (next -> improve the drawings)

---
+++ scanner.js
@@ -0,0 +1,24 @@
+var scanner_db= [{
+"id":1,
+"name": "P20",
+"company": "Leica",
+"beam_divergence [mrad]": 0.2,
+"beam_diameter [mm]": 2.8,
+"available resolutioin @10m [mm]": [0.8,1.6,3.1,6.3,12.5,25,50],
+"step_size": null
+},
+{
+"id": 2,
+"name": "Focus3d",
+"company": "Faro",
+"beam_divergence [mrad]": 0.19,
+"beam_diameter [mm]": 3,
+"available resolutioin @10m [mm]": null,
+"step_size" : 0.009 //degree
+
+
+}
+
+
+
+]
