780d4cf90b9c175c45500275b273afaa1759a20e
patorf <philippatorf@gmail.com>
2018-04-04 11:13:24 +0200

    add P40

--- scanner.js
+++ scanner.js
@@ -7,6 +7,15 @@ var scanner_db= [{
 "available resolutioin @10m [mm]": [0.8,1.6,3.1,6.3,12.5,25,50],
 "step_size": null
 },
+var scanner_db= [{
+"id":2,
+"name": "P40",
+"company": "Leica",
+"beam_divergence [mrad]": 0.23,
+"beam_diameter [mm]": 3.5,
+"available resolutioin @10m [mm]": [0.8,1.6,3.1,6.3,12.5,25,50],
+"step_size": null
+},
 
 /*{
 "id": 2,
